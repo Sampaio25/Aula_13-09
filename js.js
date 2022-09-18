d76b5c20b461eb20344abe97779237f45cddac93
Sampaio25 <marcelinhojogador12@hotmail.com>
2022-09-18 17:48:43 -0300

    Commit___

--- js.js
+++ js.js
@@ -1,23 +1,52 @@
-$(document).ready(function() {
+$(document).ready(function(e) {
+    $.ajax({
+        type: "get",
+        url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+        data: { orderBy: "nome" },
+        dataType: "json",
+        success: function(response) {
+            $.each(response, function(indexInArray, valueOfElement) {
+                var option = "<option>" + valueOfElement.sigla + "</option>"
+                $("#uf").append(option)
+            });
+        }
+    });
 
-    $('.btn').click(function(e) {
-        e.preventDefault()
+    $('#city').hide();
+    $('#loki').hide();
 
-        let dados = $('#UF').val()
+    $("#uf").change(function(e) {
+        e.preventDefault();
+        $("#loki").empty();
+        $('#city').show();
+        $('#loki').show();
+        let uf = $("#uf").val();
+    })
 
-        let url = `https://servicodados.ibge.gov.br/api/v1/localidades/distritos`
 
-        console.log(url)
+    $('#uf').change(function(e) {
+        e.preventDefault();
+        $("#loki").empty();
+        var uf = $("#uf").val();
+
+        if (uf == 'Estados') {
+            var option = "<option>Cidades</option>"
+            $("#city").append(option)
+            return
+        }
+
 
         $.ajax({
-            type: 'GET',
-            dataType: 'JSON',
-            assync: true,
-            url: url,
-            success: function(dados) {
-                $('#Cidade').val(dados.logradouro)
+            type: "get",
+            url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + uf + "/municipios",
+            data: { orderBy: "nome" },
+            dataType: "json",
+            success: function(response) {
+                $.each(response, function(indexInArray, valueOfElement) {
+                    var option = "<option>" + valueOfElement.nome + "</option>"
+                    $("#loki").append(option)
+                });
             }
-        })
-
-    })
-})
+        });
+    });
+});
